00bfc854ccf485473d4804d717598bad3203be9d
Erfan7990 <erfanahammed990@gmail.com>
2021-11-23 14:29:22 +0600

    work with add-to-cart

--- SelfMart/items/views.py
+++ SelfMart/items/views.py
@@ -18,7 +18,26 @@ def cartitem(request):
     return redirect('showcart')
 
 def show_cart(request):
-    pass
+    if request.user.is_authenticated:
+        user = request.user
+        carts = Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount = 50.0
+        total_amount = 0.0
+        product_cart = [p for p in Cart.objects.all() if p.user == user]
+
+
+        if product_cart:
+            for p in product_cart:
+                amount += (p.quantity * p.product.discount_price)
+                totalamount = shipping_amount + amount
+
+                allPrice = {
+                    'totalamount':totalamount, 
+                    'amount' : amount ,
+                    'shippingAmount' : shipping_amount,
+                    }
+        return render(request, 'items/cart_item.html', {'carts': carts, 'allPrice': allPrice})
 
 def checkout(request):
     return render(request, 'items/checkout.html')
